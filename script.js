06ef7f877d92a50b8f4df5ae14b9a509c6d4ee26
Jordan Quinn <jpquinn605@gmail.com>
2017-10-14 12:55:33 -0600

    Fix responsiveness of h1

---
+++ script.js
@@ -0,0 +1,8 @@
+var websiteTitle = $('#website-title-input').val();
+var websiteURL = $('#website-url-input').val();
+
+
+$('#submit-button').on('click', function (){
+  console.log(websiteTitle)
+  console.log(websiteURL)
+})
