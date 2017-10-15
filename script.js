4f0ea0d413737d684e119b5276edd864b88eed7a
Michelle Hoffman <hoffman.michelle.e@gmail.com>
2017-10-15 08:40:40 -0600

    Add .prepend() to make cards appear in list when submit is clicked

--- script.js
+++ script.js
@@ -7,15 +7,29 @@
 // that the input is what we expected. 
 // Fun bug to sort out: submitting the info duplicates the underline in the card display. 
 
+// Michelle - Just putting my name in so that we can know who wrote what. 
+// I liked the idea of using .text and .html, but it changed the existing card instead of creating a new one. 
+// Katie in Mod 2, hinted towards using .append() or .prepend(), so I played with that and it worked really well!
+
+
+var cardStorage = $('#card-storage');
+
+
 $('#btn-submit').on('click', function () {
   var websiteTitleInput = $('#website-title-input').val();
   var websiteURLInput = $('#website-url-input').val();
-  var websiteTitleCard = $('.website-title')
-  var websiteLinkCard = $('#website-link')
-  console.log(websiteTitleInput);
-  console.log(websiteURLInput); 
-  $(websiteTitleCard).text(websiteTitleInput);
-  $(websiteLinkCard).html('<a href="https://www." '+websiteURLInput+' id="website-link">'+websiteURLInput+'</a>');
-  console.log(websiteTitleCard);
-  console.log(websiteLinkCard);
+
+  cardStorage.prepend(
+    '<article class="card"><h2 class="website-title">' + websiteTitleInput + '</h2><a href="websiteURLInput" id="website-link">' + websiteURLInput + '</a><button id="read-button">Read</button><button id="delete-button">Delete</button></article');
+  
+  // var websiteTitleCard = $('.website-title')
+  // var websiteLinkCard = $('#website-link')  
+  // console.log(websiteTitleInput);
+  // console.log(websiteURLInput); 
+  // // $(websiteTitleCard).text(websiteTitleInput);
+  // // $(websiteLinkCard).html('<a href="https://www." '+websiteURLInput+' id="website-link">'+websiteURLInput+'</a>');
+  // console.log(websiteTitleCard);
+  // console.log(websiteLinkCard);
+  
 });
+
