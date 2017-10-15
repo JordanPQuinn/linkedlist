624aa052b9c171c70e52b7e3d8b85618615c01cf
Jordan Quinn <jpquinn605@gmail.com>
2017-10-15 06:17:48 -0600

    Create function to pull input from input fields and change placeholder website title/url

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 
+
+
+// On submit, the inner text of the card values will change to the value of the input field.
+// Formatting is inserted on line 14 to ensure that the link maintains full functionality
+// without altering the appearance of the link on the card. The console logs are to test
+// that the input is what we expected. 
 $('#btn-submit').on('click', function () {
-  var websiteTitle = $('#website-title-input').val();
-  var websiteURL = $('#website-url-input').val();
-  console.log(websiteTitle);
-  console.log(websiteURL);
+  var websiteTitleInput = $('#website-title-input').val();
+  var websiteURLInput = $('#website-url-input').val();
+  var websiteTitleCard = $('.website-title')
+  var websiteLinkCard = $('#website-link')
+  console.log(websiteTitleInput);
+  console.log(websiteURLInput); 
+  $(websiteTitleCard).text(websiteTitleInput);
+  $(websiteLinkCard).html('<a href=https://www.' + websiteURLInput + '>' + websiteURLInput + '</a>');
+  console.log(websiteTitleCard);
+  console.log(websiteLinkCard);
 });
