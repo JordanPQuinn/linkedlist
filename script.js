3b88a3c3cabf753fe800fc5938945db60f2a8681
Jordan Quinn <jpquinn605@gmail.com>
2017-10-18 11:34:29 -0600

    Make authentication functional and fix header positioning

--- script.js
+++ script.js
@@ -1,31 +1,19 @@
-
-
-
-// On submit, the inner text of the card values will change to the value of the input field.
-// Formatting is inserted on line 14 to ensure that the link maintains full functionality
-// without altering the appearance of the link on the card. The console logs are to test
-// that the input is what we expected. 
-// Fun bug to sort out: submitting the info duplicates the underline in the card display. 
-
-// Michelle - Just putting my name in so that we can know who wrote what. 
-// Katie in Mod 2, hinted towards using .append() or .prepend(), so I played with that and it worked really well! Check it out and see what you think.
-// I was going to leave the .prepend() commented out, but now the original card isn't showing up the way it was for the initial set up. Whoops. Sorry!!
-
-var cardStorage = $('#card-storage');
-var websiteURLInput = $('#website-url-input');
-var websiteTitleInput = $('#website-title-input');
-var inputForm = $('#input-form');
-
 $('#btn-submit').on('click',  createCard);
 $('#card-storage').on('click', '.read-button', toggleReadClass);
 $('#card-storage').on('click', '.delete-button', deleteCard);
 $('#website-url-input, #website-title-input').bind('keyup', enableEnterButton);
 $('.delete-all-read-button').on('click', removeAllRead);
 
+var cardStorage = $('#card-storage');
+var websiteURLInput = $('#website-url-input');
+var websiteTitleInput = $('#website-title-input');
+var inputForm = $('#input-form');
+
 function createCard() {
   var websiteTitleInput = $('#website-title-input').val();
   var websiteURLInput = $('#website-url-input').val();
-  if(validateInput() === true){
+  console.log(isValidUrl(websiteURLInput));
+  if(validateInput() === false){
     return;
   }
   else{
@@ -51,14 +39,14 @@ function validateInput() {
   var websiteURLInput = $('#website-url-input').val();
   if (websiteTitleInput === '') {
     $('.warning').text('Please enter a valid title');
-    return true;
+    return false;
   }
-  else if (websiteURLInput === '') {
-    $('.warning').text('Please enter a valid URL');
-    return true;
+  else if (websiteURLInput === '' || isValidUrl(websiteURLInput) === false) {
+    $('.warning').text('Please enter a valid URL ending in .com');
+    return false;
   }
   else{
-    return false;
+    return true;
   }
 }
 
@@ -102,5 +90,9 @@ function removeAllRead() {
   $('.card-read').remove();
   displayCardTotal()
   displayReadTotal()
+}
+
+function isValidUrl(url) {
+  return /^(http(s)?:\/\/)?(www\.)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/.test(url);
 }
 
